908f7a91e845acaad39ee44c3ddee97e7ff690cd
Ashton Alonge <ashtonalonge14@gmail.com>
2025-12-17 19:55:15 -0800

    change listening port

--- backend/server.js
+++ backend/server.js
@@ -6088,7 +6088,6 @@ app.post('/api/google/calendar/disconnect', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
