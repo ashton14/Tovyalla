ef8543648548979d4a38be9c94fb1152ad155191
Ashton Alonge <ashtonalonge14@gmail.com>
2026-01-28 19:00:25 -0800

    rename button sub -> subcontractor

--- frontend/src/components/ProjectExpenses.jsx
+++ frontend/src/components/ProjectExpenses.jsx
@@ -800,7 +800,7 @@ function ProjectExpenses({ project, onClose }) {
                 <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
                 </svg>
-                Sub
+                Subcontractor
               </button>
               <button
                 onClick={() => {
